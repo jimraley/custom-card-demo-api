b7cc0fbe179194965d42e1cd1b7de522a7c9b9f7
jimacquire <jim@acquire.io>
2021-08-24 14:13:10 -0700

    Jim's changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,21 @@ app.get("/custom-card", (req:any,res:any, next:any) => {
     const query = JSON.parse(decodeURIComponent(JSON.stringify(req.query)));
     res.render('card-template', {params: query});
 });
+app.get("/custom-card/airline", (req:any,res:any, next:any) => {
+    const query = JSON.parse(decodeURIComponent(JSON.stringify(req.query)));
+    res.render('airline-template', {params: query});
+});
+app.get("/custom-card/automotive", (req:any,res:any, next:any) => {
+    const query = JSON.parse(decodeURIComponent(JSON.stringify(req.query)));
+    res.render('automotive-template', {params: query});
+});
+app.get("/custom-card/retail", (req:any,res:any, next:any) => {
+    const query = JSON.parse(decodeURIComponent(JSON.stringify(req.query)));
+    res.render('retail-template', {params: query});
+});
+app.get("/custom-card/finance", (req:any,res:any, next:any) => {
+    const query = JSON.parse(decodeURIComponent(JSON.stringify(req.query)));
+    res.render('finance-template', {params: query});
+});
 // exports.main = functions.https.onRequest(app);
 export const acquire = functions.https.onRequest(app);
- 
